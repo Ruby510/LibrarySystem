3b32091f9a71abb7c8fcee2cc1a6b3f344e2bac3
Ruby510 <90235599+Ruby510@users.noreply.github.com>
2024-05-04 15:13:23 -0700

    update

--- bookindex.js
+++ bookindex.js
@@ -9,11 +9,20 @@ fetch("http://localhost:3000/book")
         })
     })
 
-function td_fun({ title, isbn, author, availbility}){
+function td_fun({ cover, title, isbn, author, availability}){
     let td = document.createElement('tr');
     td.innerHTML = `
     <td class="px-6 py-4 whitespace-nowrap">
-        <span class="text-sm text-gray-500">${title}</span>
+        <div class="flex items-center">
+                <div class="flex-shrink-0 h-10 w-10">
+                    <img src="${cover}" class="h-10 w-10 rounded-full" alt="">
+                </div>
+                <div class="ml-4">
+                    <div class="text-sm font-medium text-gray-900">
+                        ${title}
+                    </div>
+                </div>
+            </div>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
         <span class="text-sm text-gray-500">${isbn}</span>
@@ -22,7 +31,7 @@ function td_fun({ title, isbn, author, availbility}){
         <span class="text-sm text-gray-500">${author}</span>
     </td>
     <td class="px-6 py-4 whitespace-nowrap">
-        <span class="text-sm text-gray-500">${availbility}</span>
+        <span class="text-sm text-gray-500">${availability}</span>
     </td>
     `;
     return td;
